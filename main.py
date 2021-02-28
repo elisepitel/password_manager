512a18c2c10942a4e7cad9c5b785262f0ed129df
iseelise <39001717+elisepitel@users.noreply.github.com>
2021-02-28 10:48:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ email_label = Label(text='Email/Username:')
 email_label.grid(column=0, row=2)
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
-email_entry.insert(0, "pitel.elise@gmail.com")
+email_entry.insert(0, "prefilled@email.com")
 
 password_label = Label(text='Password:')
 password_label.grid(column=0, row=3)
